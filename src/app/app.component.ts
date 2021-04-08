7943ef0b9134bb2a5443c0e92763f1ab4b2f6da8
Priya-bharambe <pryabharambe70@gmail.com>
2021-04-08 23:37:34 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'EventBinding';
+
+  months = ["Sunday", "Monday", "Tuesday", "Wednesday",
+    "Thursday", "Friday", "Saturday"];
+  isavailable = true;
+  myClickFunction(event: any) {
+    alert("Button is clicked");
+    console.log(event);
+  }
+  changedays(event: any) {
+    console.log("Changed month from the Dropdown");
+    console.log(event);
+  }
 }
